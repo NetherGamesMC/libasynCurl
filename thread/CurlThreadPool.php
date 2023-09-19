28de0fcc30070d5d1f01efe05832c7b0e65f1d82
Dries C <coenendries3@gmail.com>
2023-09-19 20:36:59 +0200

    Revert "Temporarily increase workers"
    This reverts commit 00709c64e5667ebdf82181db9baa410139dd9a0a.

--- thread/CurlThreadPool.php
+++ thread/CurlThreadPool.php
@@ -14,7 +14,7 @@ use function gc_collect_cycles;
 class CurlThreadPool extends AsyncPool
 {
     public const MEMORY_LIMIT = 256; // 256MB Limit
-    public const POOL_SIZE = 8; // 8 workers
+    public const POOL_SIZE = 2; // 2 workers
     public const COLLECT_INTERVAL = 1; // 1 tick
     public const GARBAGE_COLLECT_INTERVAL = 15 * 60 * 20; // 15 minutes
 
